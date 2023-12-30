8498eb1c8542d33b99cb0626fc232ffedd54b423
Anish <150104412+anish-2903@users.noreply.github.com>
2023-12-31 05:14:55 +0530

    Arithmetic.swift

---
+++ Tutorial/Operators/Arithmetic.swift
@@ -0,0 +1,9 @@
+print(1 + 2)       // equals 3 , Addition Operator
+print(5 - 3)       // equals 2 , Subtraction Operator
+print(2 * 3)       // equals 6 , Multiplication Operator
+print(10.0 / 2.5)  // equals 4.0 , Division Operator
+
+
+// The addition operator is also supported for String concatenation:
+
+print("hello, " + "world")  // equals "hello, world"
