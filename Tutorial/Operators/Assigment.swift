6acc406d80edc8e39f902052299cd91fd7cadbc7
Anish <150104412+anish-2903@users.noreply.github.com>
2023-12-31 05:23:34 +0530

    Assigment.swift

--- Tutorial/Operators/Assigment.swift
+++ Tutorial/Operators/Assigment.swift
@@ -3,10 +3,12 @@
 let b = 10
 var a = 5
 a = b
+print(a, b)
 // a is now equal to 10
 
 
 // If the right side of the assignment is a tuple with multiple values, its elements can be decomposed into multiple constants or variables at once.
 
 let (x, y) = (1, 2)
+print((x, y))
 // x is equal to 1, and y is equal to 2
