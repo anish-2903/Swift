40836ac3fa8009732ad3eca23f11a4ef829c292c
Anish <150104412+anish-2903@users.noreply.github.com>
2023-12-31 05:10:35 +0530

    Assigment.swift

---
+++ Tutorial/Operators/Assigment.swift
@@ -0,0 +1,12 @@
+// The assignment operator (a = b) initializes or updates the value of a with the value of b:
+
+let b = 10
+var a = 5
+a = b
+// a is now equal to 10
+
+
+// If the right side of the assignment is a tuple with multiple values, its elements can be decomposed into multiple constants or variables at once.
+
+let (x, y) = (1, 2)
+// x is equal to 1, and y is equal to 2
