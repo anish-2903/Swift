47b73d242ed92059f9680d639a6b176a5ec8275f
Anish <150104412+anish-2903@users.noreply.github.com>
2023-12-31 05:18:42 +0530

    Remainder.swift

---
+++ Tutorial/Operators/Remainder.swift
@@ -0,0 +1,9 @@
+// The remainder operator (a % b) works out how many multiples of b will fit inside a and returns the value that’s left over (known as the remainder).
+
+print(9 % 4)    // Remainder Operator , equals 1
+
+print(-9 % 4 )  // equals -1
+
+// The sign of b is ignored for negative values of b. This means that a % b and a % -b always give the same answer.
+
+print(9 % -4)    // Remainder Operator , equals 1
